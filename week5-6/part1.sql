d8fb01a4263432ae8ab2812d0295c15df90971b4
wafforyu <wafforyu@gmail.com>
2021-09-30 01:33:39 +0800

    add part1

---
+++ week5-6/part1.sql
@@ -0,0 +1,14 @@
+-- select all columns from the tables
+SELECT * FROM emp;
+SELECT * FROM dept;
+SELECT * FROM project;
+SELECT * FROM empprojact;
+
+-- describe table
+CALL SYSPROC.ADMIN_CMD('DESCRIBE TABLE emp');
+
+-- select specific columns from table with condition.
+SELECT firstnme, midinit, lastname, job
+FROM emp 
+WHERE job IN('CLERK','OPERATOR','FIELDREP') AND edlevel > 10;
+
