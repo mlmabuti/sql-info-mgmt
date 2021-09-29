265fa629daad3a03f3e11ad9ec3ba0d4e996d67e
wafforyu <wafforyu@gmail.com>
2021-09-30 02:25:57 +0800

    part3 aliasing

---
+++ week5-6/part3.sql
@@ -0,0 +1,6 @@
+-- AS assigns alias
+SELECT lastName AS "last name", firstNme AS "first name"
+FROM emp;
+-- SELECT lastName "last name", firstNme "first name" 
+-- not adding AS results into the same thing.
+
