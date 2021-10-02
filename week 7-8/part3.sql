4ba0ae2d5925a0bef2fdc7e26af4bd86912256c2
wafforyu <wafforyu@gmail.com>
2021-10-03 04:55:15 +0800

    cap max

--- week 7-8/part3.sql
+++ week 7-8/part3.sql
@@ -2,22 +2,22 @@
 -- use distinct to get unique values (like primary key shiz) in one or more columns
 
 SELECT DISTINCT workdept
-from emp;
+FROM emp;
 
---display max salary per dept for those with education >= 16yrs
--- salary must be 60k to 160k, sort by dept and years of educ (highest first)
+--display MAX salary per dept for those with education >= 16yrs
+-- salary must be 60k to 160k, sort BY dept and years of educ (highest first)
 
 -- MY SOL
 SELECT DISTINCT workDept, MAX(salary), edLevel
-from emp 
+FROM emp 
 WHERE (salary BETWEEN 60000 AND 160000) AND edLevel >= 16 
 GROUP BY workDept, edlevel
 ORDER BY workDept, edLevel DESC;
 
 -- SIR
-SELECT workDept, edlevel, max(salary) "max salary"
+SELECT workDept, edlevel, MAX(salary) "MAX salary"
 FROM emp 
 WHERE edlevel >=16
-group by workDept,edlevel
-HAVING max(salary) BETWEEN 60000 AND 160000
-ORDER by workDept, edLevel desc;
+GROUP BY workDept,edlevel
+HAVING MAX(salary) BETWEEN 60000 AND 160000
+ORDER BY workDept, edLevel desc;
