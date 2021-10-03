6720ebe4ec7aa4679ecd862ce3475d39d24f7134
wafforyu <wafforyu@gmail.com>
2021-10-04 06:28:26 +0800

    add comms

--- week7-8/assignment4/assigment4.sql
+++ week7-8/assignment4/assigment4.sql
@@ -1,28 +1,28 @@
--- script 1 
+-- script 1: grouped by payterm; count customers with address that have CA 
 SELECT COUNT(custNo) "Customer Count", payterm
 FROM customer
 WHERE address LIKE '%CA%'
 GROUP BY payterm;
 
--- script 2 
+-- script 2: group genders then count.
 SELECT gender, COUNT(gender) "Gender Count"
 FROM personnel
 GROUP BY gender;
 
--- script 3
+-- script 3: get highest transNo purchase.
 SELECT transNo, quantity
-from receiptDetail
+FROM receiptDetail
 ORDER BY quantity DESC;
 
 
--- script 4
+-- script 4: get total amount paid per year, group all transNos and sum their amounts.
 SELECT YEAR(paydate) "YEAR", transno, SUM(amount) "Total Payment"
 FROM payment
-WHERE amount > 999
-GROUP BY YEAR(paydate),transno
+WHERE amount > 999 -- only amounts >= 1000
+GROUP BY YEAR(paydate),transno -- must group by year and transno because there are many of those rows
 ORDER BY YEAR(paydate) DESC, SUM(amount) DESC;
 
--- script 5
+-- script 5: get how many times a product's price has been updated by counting how many prodCodes there have been.
 SELECT prodCode, COUNT(prodCode) "Changes"
 FROM pricehist
 GROUP BY prodCode
