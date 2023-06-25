2c62c5fa2e1e9bd35257588086de0f76e3a219ef
Marjolo Mabuti <72655919+mlmabuti@users.noreply.github.com>
2023-06-25 19:54:49 +0800

    Update q1.sql

--- junior_year/lab8/q1.sql
+++ junior_year/lab8/q1.sql
@@ -5,8 +5,7 @@ Use the RANK function to rank customers in descending order by the sum of extend
 */
 
 SELECT c.name, SUM(f.extcost) AS total_cost, RANK() OVER (ORDER BY SUM(f.extcost) DESC) AS cust_rank
-FROM inventory_fact f
-JOIN cust_vendor_dim c ON f.custvendorkey = c.custvendorkey
-WHERE f.transtypekey = 5
+FROM inventory_fact f, cust_vendor_dim c
+WHERE f.transtypekey = 5 AND f.custvendorkey = c.custvendorkey
 GROUP BY c.name;
 
